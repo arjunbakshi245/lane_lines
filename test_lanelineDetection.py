41f1b8582944acd946836a25f7064c5eae95fc0a
Kawal <knsbatra@gmail.com>
2021-12-17 23:16:44 +0530

    update 3 for test function

--- test_lanelineDetection.py
+++ test_lanelineDetection.py
@@ -1,4 +1,4 @@
-import cv2
+""" import cv2
 import numpy as np
 
 def test_canny(img):
@@ -113,3 +113,9 @@ while(cap.isOpened()):
 cap.release()
 cv2.destroyAllWindows()
 
+ """
+def test_uppercase():
+    assert "loud noises".upper() == "LOUD NOISES"
+
+def test_reversed():
+    assert list(reversed([1, 2, 3, 4])) == [4, 3, 2, 1]
